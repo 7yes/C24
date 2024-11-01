97cb39b47eb3ba330a5f5287bc58e21f695015a1
yesmc <7yesid@gmail.com>
2024-11-01 09:43:23 -0500

    dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    id ("io.gitlab.arturbosch.detekt")
+    id("com.google.dagger.hilt.android")
+    id ("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -33,6 +37,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        dataBinding = true
+    }
 }
 
 dependencies {
@@ -40,6 +47,41 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    val navVersion = "2.7.1"
+    val hiltVersion = "2.48.1"
+    val roomVersion = "2.6.1"
+
+    //NavComponent
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    //DaggerHilt
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
+
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.3.1")
+
+    //Picasso
+    implementation ("com.squareup.picasso:picasso:2.71828")
+
+    //Room
+    //implementation ("androidx.room:room-ktx:2.5.0") // 2.4.2
+    implementation ("androidx.room:room-runtime:$roomVersion")
+    kapt ("androidx.room:room-compiler:$roomVersion")
+
+    //DataStore
+    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+    // Location Services
+    implementation ("com.google.android.gms:play-services-location:21.1.0")
+    //livedata and coroutines
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    //ZXing
+    implementation("com.journeyapps:zxing-android-embedded:4.2.0")
+
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
---
+++ app/src/main/java/com/jesse/c24/files/Basico.kt
@@ -0,0 +1,12 @@
+package com.jesse.c24.files
+
+fun main(){
+    var short = 32500
+    var myShort = short.toShort()
+
+   for(i in myShort..myShort+300){
+       println( "$i $myShort ")
+       myShort++
+
+   }
+}
