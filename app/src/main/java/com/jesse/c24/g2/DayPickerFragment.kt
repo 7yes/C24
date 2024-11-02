0cdccc77378b2556612834305d73e7e0ba4c2b6f
yesmc <7yesid@gmail.com>
2024-11-01 21:41:38 -0500

    DayPicker

--- app/src/main/java/com/jesse/c24/g2/DayPickerFragment.kt
+++ app/src/main/java/com/jesse/c24/g2/DayPickerFragment.kt
@@ -1,9 +1,15 @@
 package com.jesse.c24.g2
 
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import com.jesse.c24.databinding.FragmentDayPickerBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,11 +24,42 @@ class DayPickerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDayPickerBinding.inflate(layoutInflater, container, false)
+        binding.etDay.setOnClickListener { showDatePickerDialog() }
         return binding.root
     }
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFrag { day, month, year -> onDateSelected(day, month, year) }
+        datePicker.show(parentFragmentManager, "datePicker")
+    }
 
+    private fun onDateSelected(day: Int, month: Int, year: Int) {
+        binding.etDay.setText("$month/$day/$year")
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+}
+
+//otra clase
+// otra clase
+class DatePickerFrag(val listener: (day: Int, month: Int, year: Int) -> Unit) : DialogFragment(),
+    DatePickerDialog.OnDateSetListener {
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        listener(dayOfMonth, month, year)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val month = c.get(Calendar.MONTH)
+        val year = c.get(Calendar.YEAR)
+
+        val picker = DatePickerDialog(activity as Context, this, year, month, day)
+        picker.datePicker.minDate = c.timeInMillis
+        picker.datePicker.maxDate = c.timeInMillis + 3*86400000
+        return picker
+    }
+
 }
